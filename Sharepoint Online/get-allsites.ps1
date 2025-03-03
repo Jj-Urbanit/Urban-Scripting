0c5fe5714b6a85d1c1ad120b4be9dd09e68b835d
Jj-Urbanit <julian@urbanit.com.au>
2025-03-03 17:14:04 +1100

    Initial commit

---
+++ Sharepoint Online/get-allsites.ps1
@@ -0,0 +1,38 @@
+﻿function Get-SPOWebs(){
+param(
+   $Url = $(throw "Please provide a Site Collection Url"),
+   $Credential = $(throw "Please provide a Credentials")
+)
+
+  $context = New-Object Microsoft.SharePoint.Client.ClientContext($Url)  
+  $context.Credentials = $Credential 
+  $web = $context.Web
+  $context.Load($web)
+  $context.Load($web.Webs)
+  $context.ExecuteQuery()
+  foreach($web in $web.Webs)
+  {
+       Get-SPOWebs -Url $web.Url -Credential $Credential 
+       $web
+  }
+}
+
+Add-Type -Path "C:\Program Files\SharePoint Online Management Shell\Microsoft.Online.SharePoint.PowerShell\Microsoft.SharePoint.Client.dll"
+Add-Type -Path "C:\Program Files\SharePoint Online Management Shell\Microsoft.Online.SharePoint.PowerShell\Microsoft.SharePoint.Client.Runtime.dll"
+$UserName = "cturner_admin@themissinglink.com.au"
+$Password = Read-Host
+$SecurePassword = $Password | ConvertTo-SecureString -AsPlainText -Force
+$SPOCredentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($UserName, $SecurePassword)
+  $context = New-Object Microsoft.SharePoint.Client.ClientContext("https://themissinglink.sharepoint.com/sites/client")  
+  $context.Credentials = $SPOCredentials 
+  $web = $context.Web
+  $context.Load($web)
+  $context.Load($web.Webs)
+  $context.ExecuteQuery()
+
+$AllWebs = Get-SPOWebs -Url 'https://themissinglink.sharepoint.com/sites/client' -Credential $SPOCredentials |select title,url 
+$AllWebs | %{ 
+write-Host Title is $_.Title
+Write-Host URL is $_.URL
+Write-Host Description is $_.Description
+}
